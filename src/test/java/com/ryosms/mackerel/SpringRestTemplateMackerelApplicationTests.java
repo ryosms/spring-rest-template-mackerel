4fe5ba5bb45eb6fd9e529164677f3d6c178668d5
ryosms <ryo.sms@gmail.com>
2017-11-30 21:51:21 +0900

    Initial commit

---
+++ src/test/java/com/ryosms/mackerel/SpringRestTemplateMackerelApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ryosms.mackerel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringRestTemplateMackerelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
